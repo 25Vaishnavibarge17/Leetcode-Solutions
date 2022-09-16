eed08438c4101ea67397415e69ca0ebf1ba2856c
Vaishnavi Barge <89184566+25Vaishnavibarge17@users.noreply.github.com>
2022-09-16 23:24:29 +0530

    Create Sort List.cpp

---
+++ LinkedList/Sort List.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+     ListNode* mergedlist(ListNode*l1,ListNode*l2)
+     {
+       ListNode*ptr=new ListNode(0);
+       ListNode*curr=ptr;
+       while(l1 && l2)
+       {
+         if((l1->val)<(l2->val))
+         {
+            curr->next=l1;
+            l1=l1->next;
+         }
+         else{
+            curr->next=l2;
+            l2=l2->next;
+         }
+          curr=curr->next;
+       }
+       if(l1)
+       {
+         curr->next=l1;
+         l1=l1->next;
+       }
+       if(l2)
+       {
+           curr->next=l2;
+           l2=l2->next; 
+       }
+        return ptr->next;
+     }
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL|| head->next==NULL) return head;
+        ListNode*temp=NULL;
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(slow && fast && fast->next)
+        {
+            temp=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        temp->next=NULL;
+        ListNode*l1=sortList(head);
+        ListNode*l2=sortList(slow);
+        return mergedlist(l1,l2);
+    }
+};
