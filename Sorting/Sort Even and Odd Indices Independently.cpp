07c80ad799c079173534cd269fb56d20ade4e51c
Vaishnavi Barge <89184566+25Vaishnavibarge17@users.noreply.github.com>
2022-09-25 14:11:00 +0530

    Create Sort Even and Odd Indices Independently.cpp

---
+++ Sorting/Sort Even and Odd Indices Independently.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+   vector<int> sortEvenOdd(vector<int>& nums) {
+        
+        
+        vector<int> even,odd;
+        
+        
+        for(int i=0;i<nums.size();i++){
+            
+            if(i%2) even.push_back(nums[i]);
+            else{
+                odd.push_back(nums[i]);
+            }
+        }
+        
+        
+        sort(odd.begin(),odd.end());
+        sort(even.begin(),even.end());
+        reverse(even.begin(),even.end());
+        
+        
+        
+        int i=0,j=0;
+        
+        vector<int> ans;
+        
+        while(i<odd.size() || j<even.size()){
+             if(i<odd.size()){
+               ans.push_back(odd[i++]);
+           }
+            if(j<even.size()){
+                ans.push_back(even[j++]);
+            }
+           
+        }
+       return ans;
+    }
+};
