254d382ddb26e04118310844b8d0bec14e12a771
Vaishnavi Barge <89184566+25Vaishnavibarge17@users.noreply.github.com>
2022-11-08 20:19:00 +0530

    Create Make The String Great.cpp

---
+++ Strings/Make The String Great.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char>stk;
+        for(int i=0;i<s.size();i++)
+        {
+            if(stk.empty())
+            {
+                stk.push(s[i]);
+            }
+            else if(abs(stk.top()-s[i])==32)
+            {
+                stk.pop();
+            }
+            else
+            {
+                stk.push(s[i]);
+            }   
+            
+        }
+        string ans;
+        while(!stk.empty())
+        {
+            ans=stk.top()+ans;
+            stk.pop();
+        }
+        return ans;
+    }
+};
+
