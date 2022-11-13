cfe9da68c3d1c7f2502a3715507d442c2044e295
Vaishnavi Barge <89184566+25Vaishnavibarge17@users.noreply.github.com>
2022-11-13 12:22:57 +0530

    Create Reverse Words in a String.cpp

---
+++ Reverse Words in a String.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+     string ans;
+     int i=0;
+     int n=s.length();
+     while(i<n)
+        {
+            while(i<n && s[i]==' ')
+            {
+                i++;
+            }
+            if(i>=n)
+               break;
+            int j=i+1;
+            while(j<n && s[j]!=' ')
+            {
+                  j++;
+            }
+            string subs=s.substr(i,j-i);
+            if(ans.length()==0)
+                ans=subs;
+            else
+                ans=subs+" "+ans;
+            i=j+1;
+        }   
+      return ans;
+    }
+};
